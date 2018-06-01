77f24dc2148f9d0c3f48c9c3105e52ade3dafd41
afrahuls <sunil.jain@afourtech.com>
2018-06-01 16:11:57 +0530

    --

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var express = require('express');
 app = express();
 
 
-app.use('/data', (req, res) => {
+app.use('/data', (req res) => {
     res.status(200).json({
         message: "Successfuly started......"
     })
-});
+})
 
 app.listen(10011, () => {
     console.log("server is being started at 10011");
