d95ff707e81f459109680791ba301f889ec45c97
sujain <sunil.jain@afourtech.com>
2018-06-01 14:47:47 +0530

    server initialized

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+var express = require('express');
+app = express();
 
+
+app.use('/data', (req, res) => {
+    res.status(200).json({
+        message: "Success"
+    })
+});
+
+app.listen(10011, () => {
+    console.log("server started at 10011");
+});
